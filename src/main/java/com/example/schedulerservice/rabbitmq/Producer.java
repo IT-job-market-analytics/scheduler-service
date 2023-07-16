352ebc5ef9de7d426a6e4531539e1949d5fc7e8f
Sergey Zhukov <zhukovsd@gmail.com>
2023-07-16 14:29:57 +0400

    feat: 2 separate methods in `Producer` to send tasks

--- src/main/java/com/example/schedulerservice/rabbitmq/Producer.java
+++ src/main/java/com/example/schedulerservice/rabbitmq/Producer.java
@@ -1,5 +1,6 @@
 package com.example.schedulerservice.rabbitmq;
 
+import com.example.schedulerservice.dto.AnalyticsBuilderServiceTaskDto;
 import com.example.schedulerservice.dto.VacancyImportScheduledTaskDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -9,14 +10,18 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class Producer {
 
-    private RabbitTemplate template;
+    private final RabbitTemplate template;
 
     public Producer(RabbitTemplate template) {
         this.template = template;
     }
 
-    public void sendMessage(String routingKey, Object object) {
-        template.convertAndSend(routingKey, object);
+    public void sendMessage(String routingKey, AnalyticsBuilderServiceTaskDto task) {
+        template.convertAndSend(routingKey, task);
+    }
+
+    public void sendMessage(String routingKey, VacancyImportScheduledTaskDto task) {
+        template.convertAndSend(routingKey, task);
     }
 
 }
